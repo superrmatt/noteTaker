264f93b3f3826903834889f27c8eb5b2cab9552b
Matt Dambra <mpdambra@gmail.com>
2020-02-14 17:06:44 -0500

    and that should do it bud

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ let path = require('path');
 let app = express();
 let PORT = process.env.PORT || 3000;
 
+let count = 0;
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(express.static(__dirname));
+app.use(express.json());
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -15,8 +17,59 @@ app.get("/", function(req, res) {
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-})
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.json(getData());
+});
+
+app.post("/api/notes", function(req, res) {
+    let dbJSON = getData();
+    if (dbJSON === false) 
+        dbJSON = [];
+    let newNote = req.body;
+    count++;
+    let id = count;
+    newNote.id = id;
+    dbJSON.push(newNote);
+    toStringAndWrite(dbJSON);
+    res.json(dbJSON);
+});
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("Listening on " + PORT);
 });
+
+app.delete("/api/notes/:id", function(req, res) {
+    let id = parseInt(req.params.id);
+    let dbJSON = getData();
+
+    for(let i = 0; i < dbJSON.length; i++) {
+        if(dbJSON[i].id === id){
+            let newArr = dbJSON.filter(note => {
+                return note.id !== id;
+            });
+        }
+    }
+
+    toStringAndWrite(newArr);
+    res.json(newArr);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+function getData() {
+    let dbString = fs.readFileSync("db/db.json", 'utf8');
+    if (dbString !== ""){
+        let dbJSON = JSON.parse(dbString);
+        return dbJSON;
+    }
+    return false;
+}
+
+function toStringAndWrite(array){
+    let  dbString = JSON.stringify(array);
+    fs.writeFileSync("db/db.json", dbString, "utf-8");
+}
